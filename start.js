2c45575481c6271d9680448673315978cd971e0e
ferrymen <39427935+ferrymen@users.noreply.github.com>
2020-03-25 06:58:22 +0800

    update

--- start.js
+++ start.js
@@ -1,39 +1,39 @@
 var childCprocess = require('child_process')
-var path = require('path')
+// var path = require('path')
 var exec = childCprocess.exec
 
-exec('npm install', { cwd: path.resolve('./packages/client/web') }, function(
-  err
-) {
-  if (err) {
-    throw err
-  }
-  console.log('[web] Dependence installed...')
-  exec('npm start', { cwd: path.resolve('./packages/client/web') })
-})
+// exec('npm install', { cwd: path.resolve('./packages/client/web') }, function(
+//   err
+// ) {
+//   if (err) {
+//     throw err
+//   }
+//   console.log('[web] Dependence installed...')
+//   exec('npm start', { cwd: path.resolve('./packages/client/web') })
+// })
 
-exec('npm install', { cwd: path.resolve('./packages/client/public') }, function(
-  err
-) {
-  if (err) {
-    throw err
-  }
-  console.log('[public] Dependence installed...')
-  exec('npm start', { cwd: path.resolve('./packages/client/public') })
-})
+// exec('npm install', { cwd: path.resolve('./packages/client/public') }, function(
+//   err
+// ) {
+//   if (err) {
+//     throw err
+//   }
+//   console.log('[public] Dependence installed...')
+//   exec('npm start', { cwd: path.resolve('./packages/client/public') })
+// })
 
-exec('npm install', { cwd: path.resolve('./packages/server/api') }, function(
-  err
-) {
-  if (err) {
-    throw err
-  }
-  console.log('[api] Dependence installed...')
-  exec('npm start', { cwd: path.resolve('./packages/server/api') })
-})
+// exec('npm install', { cwd: path.resolve('./packages/server/api') }, function(
+//   err
+// ) {
+//   if (err) {
+//     throw err
+//   }
+//   console.log('[api] Dependence installed...')
+//   exec('npm start', { cwd: path.resolve('./packages/server/api') })
+// })
 
 // execSync('npx lerna bootstrap')
 
-// execSync('npx lerna run start')
+exec('npx lerna run start')
 
 exec('npx lerna run bind')
