b6a6551943a1c8ee5a05db1e9743b52edc9f8ed0
ferrymen <39427935+ferrymen@users.noreply.github.com>
2020-03-25 08:32:54 +0800

    update

--- start.js
+++ start.js
@@ -1,38 +1,6 @@
 var childCprocess = require('child_process')
 // var path = require('path')
-var exec = childCprocess.execSync
-
-// exec('npm install', { cwd: path.resolve('./packages/client/web') }, function(
-//   err
-// ) {
-//   if (err) {
-//     throw err
-//   }
-//   console.log('[web] Dependence installed...')
-//   exec('npm start', { cwd: path.resolve('./packages/client/web') })
-// })
-
-// exec('npm install', { cwd: path.resolve('./packages/client/public') }, function(
-//   err
-// ) {
-//   if (err) {
-//     throw err
-//   }
-//   console.log('[public] Dependence installed...')
-//   exec('npm start', { cwd: path.resolve('./packages/client/public') })
-// })
-
-// exec('npm install', { cwd: path.resolve('./packages/server/api') }, function(
-//   err
-// ) {
-//   if (err) {
-//     throw err
-//   }
-//   console.log('[api] Dependence installed...')
-//   exec('npm start', { cwd: path.resolve('./packages/server/api') })
-// })
-
-// execSync('npx lerna bootstrap')
+var exec = childCprocess.exec
 
 exec('npx lerna run start')
 
