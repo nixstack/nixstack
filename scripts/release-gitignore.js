652e418b7b75bc84e8abf7653c8ae91cea5bf92a
ferrymen <39427935+ferrymen@users.noreply.github.com>
2020-03-26 20:42:03 +0800

    update

--- scripts/release-gitignore.js
+++ scripts/release-gitignore.js
@@ -5,5 +5,8 @@ var webPackage = path.resolve('packages/client/web')
 var publicPackage = path.resolve('packages/client/public')
 
 shelljs.echo('src').toEnd(path.resolve(`${webPackage}/.gitignore`))
-shelljs.echo('package.json').toEnd(path.resolve(`${webPackage}/.gitignore`))
-shelljs.sed('-i', /\/static\//, '', path.resolve(`${publicPackage}/.gitignore`))
+// shelljs.echo('package.json').toEnd(path.resolve(`${webPackage}/.gitignore`))
+// shelljs.sed('-i', /\/static\//, '', path.resolve(`${publicPackage}/.gitignore`))
+
+shelljs.mv(`${webPackage}/package.json`, `${webPackage}/package.json.bak`)
+shelljs.exec(`git add -f ${webPackage}/static`)
