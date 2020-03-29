c78b984b482aaae994990484f0ada57bdbb2ef7b
root <root@localhost.localdomain>
2020-03-29 07:48:36 -0400

    update

--- scripts/release-gitignore.js
+++ scripts/release-gitignore.js
@@ -2,11 +2,11 @@ var shelljs = require('shelljs')
 var path = require('path')
 
 var webPackage = path.resolve('packages/client/app-room')
-var publicPackage = path.resolve('../__public__')
+var public = path.resolve('../__public__')
 
 shelljs.echo('src').toEnd(path.resolve(`${webPackage}/.gitignore`))
 // shelljs.echo('package.json').toEnd(path.resolve(`${webPackage}/.gitignore`))
 // shelljs.sed('-i', /\/static\//, '', path.resolve(`${publicPackage}/.gitignore`))
 
 shelljs.mv(`${webPackage}/package.json`, `${webPackage}/package.json.bak`)
-shelljs.exec(`git add -f ${publicPackage}/static`)
+shelljs.exec(`git add -f ${public}`)
